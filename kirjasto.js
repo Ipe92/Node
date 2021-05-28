eb915454b3d65a709798e8436d42721a69343dbc
Iippo Leppänen <21137181@edu.riveria.fi>
2021-05-28 13:57:03 +0300

    valinnat pitäisi saada toimiaan (kaikki toimii)

---
+++ kirjasto.js
@@ -0,0 +1,79 @@
+const yargs = require("yargs");
+const kirjat = require("./kirjat");
+
+yargs.command({
+    command: "lisaa",
+    description: "Lisää uusi kirja",
+    builder: {
+        nimi: {
+            type: "string",
+            demandOption: true,
+            describe: "Kirjan nimi",
+        },
+        kirjoittaja: {
+            type: "array",
+            demandOption: true,
+            describe: "Kirjan kirjoittajat",
+        },
+        valinta: {
+            demandOption: true,
+            choices: ["kaikki", "Fantasia", "Sota", "Tiede"],
+            describe: "Listataan kirjat valinnan perusteella",
+        },
+    },
+    handler(argv) {
+        kirjat.lisaaKirja(argv.nimi, argv.kirjoittaja, argv.valinta);
+    },
+});
+
+yargs.command({
+    command: "listaa",
+    description: "Listaa kirjat",
+    builder: {
+        valinta: {
+            demandOption: true,
+            choices: ["kaikki", "Fantasia", "Sota", "Tiede"],
+            describe: "Listataan kirjat valinnan perusteella",
+        },
+    },
+    handler(argv) {
+        kirjat.listaaKirja(argv.valinta);
+    },
+});
+
+yargs.command({
+    command: "poista",
+    description: "Poista kirja",
+    builder: {
+        nimi: {
+            type: "string",
+            demandOption: true,
+            describe: "Kirjan nimi",
+        },
+        kirjoittaja: {
+            type: "string",
+            demandOption: true,
+            describe: "Kirjan kirjoittaja",
+        },
+    },
+    handler(argv) {
+        kirjat.poistaKirja(argv.nimi, argv.kirjoittaja);
+    },
+});
+
+yargs.command({
+    command: "hae",
+    description: "Hae kirjoittajaa",
+    builder: {
+        kirjoittaja: {
+            type: "string",
+            demandOption: true,
+            describe: "Hae kirjoittajaa nimellä",
+        },
+    },
+    handler(argv) {
+        kirjat.haeKirjoittaja(argv.kirjoittaja);
+    },
+});
+
+yargs.parse();
---
+++ kirjat.js
@@ -0,0 +1,96 @@
+const fs = require("fs");
+
+const lisaaKirja = (nimi, kirjoittaja, valinta) => {
+    const kirjat = lataaKirjat();
+    const onOlemassa = kirjat.find(
+        (kirja) => kirja.nimi === nimi && kirja.kirjoittaja === kirjoittaja && kirja.valinta === valinta,
+    );
+    if (!onOlemassa) {
+        kirjat.push({
+            nimi,
+            kirjoittaja,
+            valinta,
+        });
+        tallennaKirjat(kirjat);
+        console.log("Kirja lisätty kirjastoon.");
+    } else {
+        console.log("Kirja on jo olemassa, ei lisätty kirjastoon.");
+    }
+};
+
+const poistaKirja = (nimi, kirjoittaja) => {
+    const kirjat = lataaKirjat();
+    const onOlemassa = kirjat.find((kirja) => kirja.nimi === nimi && kirja.kirjoittaja === kirjoittaja);
+    if (!onOlemassa) {
+        console.log("Kirjaa ei ole, joten sitä ei voi poistaa");
+    } else {
+        kirjat.splice(
+            {
+                nimi,
+                kirjoittaja,
+            },
+            1,
+        );
+        tallennaKirjat(kirjat);
+        console.log(`Kirja nimeltään: ${kirjat.nimi} poistettu kirjastosta`);
+    }
+};
+
+const haeKirjoittaja = (kirjoittaja) => {
+    const kirjat = lataaKirjat();
+    const onOlemassa = kirjat.find((kirja) => kirja.nimi === nimi && kirja.kirjoittaja === kirjoittaja);
+    if (onOlemassa === "") {
+        console.log("Kirjoittajaa ei löytynyt");
+    } else {
+        console.log(`Kirjoittaja löytyi: ${onOlemassa}`);
+    }
+};
+
+// valinnat pitäisi saada toimiaan (kaikki toimii)
+const listaaKirja = (valinta) => {
+    const kirjat = lataaKirjat();
+    console.log(`Tässä lista ${valinta}`);
+
+    if (valinta === "kaikki") {
+        kirjat.forEach((valinta) => {
+            console.log(valinta);
+        });
+    } else if (valinta === "Fantasia") {
+        kirjat.forEach((valinta) => {
+            if (valinta.includes(valinta)) {
+                console.log(valinta);
+            }
+            console.log(valinta);
+        });
+    } else if (valinta === "Sota") {
+        kirjat.forEach((valinta) => {
+            console.log(valinta);
+        });
+    } else if (valinta === "Tiede") {
+        kirjat.forEach((valinta) => {
+            console.log(valinta);
+        });
+    }
+};
+
+const tallennaKirjat = (kirjat) => {
+    const kirjatJSON = JSON.stringify(kirjat);
+    fs.writeFileSync("kirjat.json", kirjatJSON);
+};
+
+const lataaKirjat = () => {
+    try {
+        const dataBuffer = fs.readFileSync("kirjat.json");
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    } catch (e) {
+        return [];
+    }
+};
+
+module.exports = {
+    lisaaKirja: lisaaKirja,
+    listaaKirja: listaaKirja,
+    poistaKirja: poistaKirja,
+    haeKirjoittaja: haeKirjoittaja,
+};
